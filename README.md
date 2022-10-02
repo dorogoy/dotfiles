63da47537642d125079efc08feee562645606886
Sergio Rael <sergio.rael@proton.me>
2022-10-02 13:11:10 +0200

    Add README file

---
+++ README.md
@@ -0,0 +1,10 @@
+# Dotfiles
+
+My personal dotfiles to be used with [chezmoi](https://www.chezmoi.io). To install:
+
+```bash
+sh -c "$(curl -fsLS https://chezmoi.io/get)" -- init --apply dorogoy
+```
+
+More info: https://www.chezmoi.io
+
--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -3,4 +3,4 @@
 if test -f "/etc/profile.d/vte-2.91.sh"; then
 	sudo ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
 fi
-sudo apt install tilix editorconfig fonts-firacode fonts-powerline
+sudo apt install tilix kakoune micro editorconfig fonts-firacode fonts-powerline
