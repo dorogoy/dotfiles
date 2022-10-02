86d8d934593baf4cf6af847e07880aaba44ddb58
Sergio Rael <sergio.rael@proton.me>
2022-10-02 20:09:02 +0200

    Install MesloLGS fonts

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+cd ~/.local/share/fonts \
+  && wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf \
+  && wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf \
+  && wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf \
+  && wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+
 if test -f "/etc/profile.d/vte-2.91.sh"; then
 	sudo ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
 fi
