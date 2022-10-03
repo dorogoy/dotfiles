b60417447810641392f43c94644789bfa4f93604
Sergio Rael <sergio.rael@proton.me>
2022-10-03 19:06:56 +0200

    Ask for the name

--- run_once_install-packages.sh
+++ run_once_install-packages.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-mkdir ~/.local/share/fonts
+mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts \
-  && wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf \
-  && wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf \
-  && wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf \
-  && wget https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
+  && wget -q -nc https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf \
+  && wget -q -nc https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold.ttf \
+  && wget -q -nc https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Italic.ttf \
+  && wget -q -nc https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Bold%20Italic.ttf
 
 if test -f "/etc/profile.d/vte-2.91.sh"; then
 	sudo ln -s /etc/profile.d/vte-2.91.sh /etc/profile.d/vte.sh
